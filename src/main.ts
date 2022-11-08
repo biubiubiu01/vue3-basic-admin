35427fe6507d5b52689fdaa123e12fb9e85a800e
biubiubiu01 <1979420758@qq.com>
2022-11-08 15:18:52 +0800

    test: 测试提交

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import { setupPinia } from "./stores";
+import router from "./router/index";
 
 const app = createApp(App);
 
 const setupPlugins = () => {
-  //注册pinia
-  setupPinia(app);
+    // 注册pinia
+    setupPinia(app);
 };
 
 setupPlugins();
-app.mount("#app");
+app.use(router).mount("#app");
