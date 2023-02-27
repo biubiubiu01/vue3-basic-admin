62bf0c7e31dfef8b6b1229d7b6754cf50d45f365
gist006 <1979420758@qq.com>
2023-02-27 19:27:51 +0800

    fix: change

--- src/mockProdServer.ts
+++ src/mockProdServer.ts
@@ -1,15 +1,19 @@
 import { createProdMockServer } from "vite-plugin-mock/es/createProdMockServer";
+import log from "./mock/controller/log";
+import system from "./mock/controller/system";
+import test from "./mock/controller/test";
+import user from "./mock/controller/user";
 
-const getMockModule = () => {
-    const modules = import.meta.glob("./mock/controller/**/*.ts", { eager: true, import: "default" });
-    const mockModules: any[] = [];
-    Object.values(modules).forEach((value: any) => {
-        const moduleList = Array.isArray(value) ? [...(value as any)] : [value];
-        mockModules.push(...moduleList);
-    });
-    return mockModules;
-};
+// const getMockModule = () => {
+//     const modules = import.meta.glob("./mock/controller/**/*.ts", { eager: true, import: "default" });
+//     const mockModules: any[] = [];
+//     Object.values(modules).forEach((value: any) => {
+//         const moduleList = Array.isArray(value) ? [...(value as any)] : [value];
+//         mockModules.push(...moduleList);
+//     });
+//     return mockModules;
+// };
 
 export function setupProdMockServer() {
-    createProdMockServer(getMockModule());
+    createProdMockServer([...log, ...system, ...test, ...user]);
 }
