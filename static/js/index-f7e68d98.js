80ab319ae5bec40133303d2b8cca10dca0730b13
biubiubiu01 <1979420758@qq.com>
2023-04-23 09:41:38 +0800

    add

---
+++ static/js/index-f7e68d98.js
@@ -0,0 +1,1 @@
+import{d as s,bU as r,aj as n,o as c,c as u,ba as p,k as i}from"./index-dea3e1d5.js";const _={class:"page-redirect-container"},h=s({__name:"index",setup(d){const e=p(),t=i(),{open:o,close:a}=r({modal:!1});return o(),setTimeout(()=>{a(),t.replace({path:e.query.path,query:{...n(e.query,"path")}})},700),(l,m)=>(c(),u("div",_))}});export{h as default};
