4fc119c059de2bd4183803c95f0b9648e6752eca
biubiubiu01 <1979420758@qq.com>
2023-09-21 17:10:26 +0800

    fix

--- static/js/dict-58413fb1.js
+++
@@ -1,1 +0,0 @@
-import{cq as e}from"./index-b24fdcf6.js";var a=(t=>(t.GET_DICT_KEY="/dict/key",t.GET_DICT_DATA="/dict/data",t))(a||{});const _=t=>e.get("/dict/key",t),o=t=>e.get("/dict/data",t),r=Object.freeze(Object.defineProperty({__proto__:null,Api:a,getDictKeyList:_,getDictData:o},Symbol.toStringTag,{value:"Module"}));export{r as _,o as a,_ as g};
